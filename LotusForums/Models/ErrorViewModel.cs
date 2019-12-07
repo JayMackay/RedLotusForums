980b38ac9116a912255ab5fb0504370c62578d7c
Jared Mackay <jared.mackay1994@live.com>
2019-12-07 19:48:17 +0000

    Add project files.

---
+++ LotusForums/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LotusForums.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
